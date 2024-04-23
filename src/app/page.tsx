b491597a8beabeb48a86eb6ee2a71cb5fd83e056
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-04-23 18:09:18 +0530

    initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-4 w-max m-2">
+      Hello world this is ClassPlanner Pro
+      <Button variant={"outline"}>Click Me</Button>
+    </div>
+  );
+}
