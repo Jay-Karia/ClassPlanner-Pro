2fd18d4ec5086d909dc89ea1c00cd597173f732c
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-04-23 19:13:22 +0530

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 w-max m-2">
-      Hello world this is ClassPlanner Pro
+      <Image src="/logo.svg" alt="ClassPlanner Pro" width={40} height={40} />
+      ClassPlanner Pro
       <Button variant={"outline"}>Click Me</Button>
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        hover: "bg-primary text-primary-foreground shadow hover:bg-primary/90 hover:-translate-y-1 hover:transition-transform hover:delay-200 hover:ease-in transition-transform delay-200 ease-in",
       },
       size: {
         default: "h-9 px-4 py-2",
