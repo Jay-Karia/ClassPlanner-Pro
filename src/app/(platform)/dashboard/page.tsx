b95965625e9afd7600cc315c259d3e2813f60a68
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-04-26 09:06:18 +0530

    [setup] zustand

--- src/app/(platform)/dashboard/page.tsx
+++ src/app/(platform)/dashboard/page.tsx
@@ -6,13 +6,17 @@ import { Button } from "@/components/ui/button";
 import { usePathname, useSearchParams } from 'next/navigation'
 import {useEffect} from 'react'
 
+import {useStore} from "@/store"
+
 export default function DashboardPage() {
 
     // rendering the content depending on the URL
     const pathname = usePathname()
     const searchParams = useSearchParams()
 
-    
+    // store
+    const bears = useStore((state) => state.bears)
+    const increment = useStore((state)=>state.increasePopulation)
 
     useEffect(()=> {
     }, [pathname, searchParams])
@@ -20,7 +24,8 @@ export default function DashboardPage() {
     return (
         <div className="flex flex-col w-full">
             {/* Main Content */}
-            
+            {bears}
+            <Button onClick={increment}>Add</Button>
         </div>
     );
 }
